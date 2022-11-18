3cc664d203767901f36798170e3576bb224c0d3b
MMDCCJ <654371320@qq.com>
2022-11-19 03:00:20 +0800

    完

--- router_handler/artcate.js
+++ router_handler/artcate.js
@@ -28,7 +28,50 @@ exports.addArticleCates = (req, res) => {
         db.query(sql, req.body, (err, results) => {
             if (err) return res.cc(err)
             if (results.affectedRows != 1) return res.cc("添加失败")
-            res.cc("添加成功",0)
+            res.cc("添加成功", 0)
+        })
+    })
+
+}
+
+exports.deleteCateByid = (req, res) => {
+    const sql = "update article SET is_delete= 1 WHERE id=?"
+    db.query(sql, req.params.id, (err, results) => {
+        if (err) return res.cc(err)
+        if (results.affectedRows != 1) return res.cc("删除错误")
+        res.cc("删除成功", 0)
+    })
+}
+
+exports.getArticleByid = (req, res) => {
+    const sql = "SELECT name,alias FROM article WHERE id = ? AND is_delete=0"
+    db.query(sql, req.params.id, (err, results) => {
+        if (err) return res.cc(err)
+        if (results.length != 1) return res.cc("未找到该id数据")
+        res.send({
+            status: 0,
+            message: "获取成功",
+            data: results
+        })
+    })
+}
+
+exports.UpdateArticleByid = (req, res) => {
+    const checkSQL = "SELECT name,alias,id FROM article WHERE id<>? AND name=? OR alias=?"
+    const sql = "UPDATE article SET name=?,alias=? WHERE id=?"
+    const data = req.body
+    db.query(checkSQL, [data.id, data.name, data.alias], (err, results) => {
+        console.log(results);
+        if (err) return res.cc(err)
+        if (results.length == 1) {
+            if (results[0].name == data.name) return res.cc("该名称已被占用")
+            if (results[0].alias == data.alias) return res.cc("别名已被占用")
+        }
+        if (results.length == 2) return res.cc("名称与别名均被占用")
+        db.query(sql, [data.name, data.alias, data.id], (err, results) => {
+            if (err) return res.cc(err)
+            if (results.affectedRows != 1) return res.cc("更新失败，请重试")
+            res.cc("更改成功", 0)
         })
     })
 
--- schema/artcate.js
+++ schema/artcate.js
@@ -1,10 +1,30 @@
+const expressJoi = require('@escook/express-joi')
 const joi = require('joi')
 
 const name = joi.string().required()
 const alias = joi.string().alphanum().required()
-
+const id = joi.number().integer().min(1).required()
 exports.add_cate_schema = {
     body:{
+        name,
+        alias
+    }
+}
+exports.delete_cate_schema = {
+    params:{
+        id
+    }
+}
+
+exports.get_cateByid_schema = {
+    params:{
+        id
+    }
+}
+
+exports.update_cateByid_schema = {
+    body:{
+        id,
         name,
         alias
     }
